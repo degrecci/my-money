dde081b8c2c361a2346586fb2030e0c7a2fc6905
degrecci <hdegrecci@gmail.com>
2018-03-20 20:11:33 -0300

    Setup inicial :tada:

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// static files
+app.use(express.static('public'));
+// start server
+app.listen(process.env.PORT || 5000);
